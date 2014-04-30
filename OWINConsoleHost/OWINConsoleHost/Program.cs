4f2d5bc4f33f7150368b2a0a480aba0db1dd4f47
githubUser919 <duk444@gmail.com>
2014-04-30 10:45:45 +0100

    Basic Console-hosted http server using package Microsoft.Owin.SelfHost

---
+++ OWINConsoleHost/OWINConsoleHost/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.Owin.Hosting;
+using Owin;
+
+namespace OWINConsoleHost
+{
+    using AppFunc = Func<IDictionary<string, object>, Task>;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var uri = "http://localhost:8082";
+            using (WebApp.Start<Startup1>(uri))
+            {
+                Console.WriteLine("Started.");
+                Console.ReadKey();
+                Console.WriteLine("Stopping.");
+
+            }
+        }
+    }
+
+    public class Startup1
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.Run(context =>
+            {
+                context.Response.ContentType = "text/plain";
+                return context.Response.WriteAsync("Hello");
+            });
+        }
+    }
+}
