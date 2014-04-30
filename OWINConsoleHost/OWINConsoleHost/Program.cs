f684c704dba6fec5cff2316ea9912d3c11aaaafe
githubUser919 <duk444@gmail.com>
2014-04-30 11:27:48 +0100

    Use low level custom component instead of app.UserWelcomePage() or app.Run

--- OWINConsoleHost/OWINConsoleHost/Program.cs
+++ OWINConsoleHost/OWINConsoleHost/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Threading.Tasks;
 using Microsoft.Owin.Hosting;
 using Owin;
@@ -27,13 +28,40 @@ namespace OWINConsoleHost
     {
         public void Configuration(IAppBuilder app)
         {
+            // Use low level custom component instead of app.UserWelcomePage() or app.Run...
+            app.Use<HellowWorldComponent>();
+
+            /*
+            
             app.UseWelcomePage();
 
-            //app.Run(context =>
-            //{
-            //    context.Response.ContentType = "text/plain";
-            //    return context.Response.WriteAsync("Hello");
-            //});
+            app.Run(context =>
+            {
+                context.Response.ContentType = "text/plain";
+                return context.Response.WriteAsync("Hello");
+            });
+            
+             */
+        }
+    }
+
+    public class HellowWorldComponent
+    {
+        AppFunc _next;
+
+        public HellowWorldComponent(AppFunc next)
+        {
+            _next = next;
+        }
+
+        public Task Invoke(IDictionary<string, object> environment )
+        {
+            var response = environment["owin.ResponseBody"] as Stream;
+
+            using (var writer = new StreamWriter(response))
+            {
+                return writer.WriteAsync("Hello!!");
+            }
         }
     }
 }
