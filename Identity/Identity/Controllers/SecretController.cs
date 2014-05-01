0b8ab8db17e543d7d8afd0f38d87f94aad69198a
githubUser919 <duk444@gmail.com>
2014-05-01 15:00:30 +0100

    Demonstrate Authorization, change db name, db file name

---
+++ Identity/Identity/Controllers/SecretController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Identity.Controllers
+{
+    /// <summary>
+    /// Illustrates Authentication and 
+    /// </summary>
+    [Authorize]
+    public class SecretController : Controller
+    {
+        public ContentResult Secret()
+        {
+            return Content("This is SECRET content!");
+        }
+
+        [AllowAnonymous]
+        public ContentResult Overt()
+        {
+            return Content("This isn't secret content.");
+        }
+	}
+}
