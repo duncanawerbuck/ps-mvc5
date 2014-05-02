5eba5473666018eec4a636ce222a858059ec45f2
githubUser919 <duk444@gmail.com>
2014-05-02 16:23:33 +0100

    +Books solution w/ Books.Web MVC app. Packages updated.

---
+++ EntityFramework/Books/Books.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Books.Web.MvcApplication" Language="C#" %>
---
+++ EntityFramework/Books/Books.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Books.Web.Startup))]
+namespace Books.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
