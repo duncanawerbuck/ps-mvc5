4c1a62e9aac3704d8a737ce7c5eec8a41cf624b8
githubUser919 <duk444@gmail.com>
2014-05-06 14:47:05 +0100

    Performance counter helpers

---
+++ PerfSerf/PerfSerf/Counters/PerfCounterService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PerfSerf.Counters
+{
+    public class PerfCounterService
+    {
+        public PerfCounterService()
+        {
+            _counters = new List<PerfCounterWrapper>
+            {
+                new PerfCounterWrapper("Processor", "Processor", "% Processor Time", "_Total"),
+                new PerfCounterWrapper("Paging", "Memory", "Pages/sec"),
+                new PerfCounterWrapper("Disk", "PhysicalDisk", "% Disk Time", "_Total")
+            };
+        }
+
+        public dynamic GetResults()
+        {
+            return _counters.Select(c => new {name = c.Name, value = c.Value});
+        }
+
+        private List<PerfCounterWrapper> _counters;
+    }
+}
---
+++ PerfSerf/PerfSerf/Counters/PerfCounterWrapper.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+
+namespace PerfSerf.Counters
+{
+    public class PerfCounterWrapper
+    {
+        public PerfCounterWrapper(string name, string category, string counter, string instance = "")
+        {
+            _counter = new PerformanceCounter(category, counter, instance, readOnly: true);
+
+            Name = name;
+        }
+
+        public string Name { get; set; }
+
+        public float Value
+        {
+            get { return _counter.NextValue(); }
+        }
+
+        PerformanceCounter _counter;
+    }
+
+}
