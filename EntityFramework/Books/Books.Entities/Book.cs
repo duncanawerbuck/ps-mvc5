ff71735d207c75f0cc10bd8f9809fc7364e7cc46
githubUser919 <duk444@gmail.com>
2014-05-06 10:44:38 +0100

    Added Books.Entities project w/ Book entity

---
+++ EntityFramework/Books/Books.Entities/Book.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Books.Entities
+{
+    public class Book
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Title { get; set; }
+    }
+}
