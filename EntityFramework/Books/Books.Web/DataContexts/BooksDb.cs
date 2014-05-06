3941dc4dd1ca59361f1d627e953a24913150d403
githubUser919 <duk444@gmail.com>
2014-05-06 10:49:45 +0100

    BooksDb DbContext created in Books.Web

---
+++ EntityFramework/Books/Books.Web/DataContexts/BooksDb.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using Books.Entities;
+
+namespace Books.Web.DataContexts
+{
+    public class BooksDb : DbContext
+    {
+        public DbSet<Book> Book { get; set; }
+    }
+}
