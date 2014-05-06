e5898666b970b072b9b60e14004ddf1c24c1f6fe
githubUser919 <duk444@gmail.com>
2014-05-06 13:53:25 +0100

    +Startup class and Hub class

---
+++ PerfSerf/PerfSerf/Hubs/PerfHub.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace PerfSerf.Hubs
+{
+    public class PerfHub : Hub
+    {
+        public void Hello()
+        {
+            Clients.All.hello("Hi!");
+        }
+    }
+}
---
+++ PerfSerf/PerfSerf/Startup.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(PerfSerf.Startup))]
+
+namespace PerfSerf
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
